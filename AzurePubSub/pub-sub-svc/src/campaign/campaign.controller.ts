408ae1d5f0b93de023e6bae6c7c914d3f15514b9
Carlos Gutierrez <carlos.gutierrez>
2022-09-02 00:18:11 -0500

    Genera el QR

--- AzurePubSub/pub-sub-svc/src/campaign/campaign.controller.ts
+++ AzurePubSub/pub-sub-svc/src/campaign/campaign.controller.ts
@@ -2,20 +2,23 @@ import { Controller,Get } from '@nestjs/common';
 import { normalize } from 'path';
 import { AppService } from '../app.service';
 import { campaignqrrequest } from './dto/base-qr.dto';
+var QRCode = require('qrcode')
+
 var tipo=1;
 @Controller('campaign')
 export class CampaignController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): campaignqrrequest {
+  async getHello(): Promise<campaignqrrequest> {
     var hora =new Date().toLocaleString();
     console.log('hora -> '+hora)
     var resultado={
         "idpetition": "WERUIJA",
         "Qrimage":"https://ecoverseresources.blob.core.windows.net/images/angular.gif",
         "tipo":tipo,
-        "hora":hora
+        "hora":hora,
+        "qrurl":""
     }
     if(tipo===1){
       console.log('Tipo 1');
@@ -27,8 +30,22 @@ export class CampaignController {
       tipo=1;
       resultado.Qrimage="https://ecoverseresources.blob.core.windows.net/images/images.png";
     }
+    QRCode.toFile('filename.png', JSON.stringify(resultado), {
+      color: {
+        dark: '#00F',  // Blue dots
+        light: '#0000' // Transparent background
+      }
+    }, function (err) {
+      if (err) throw err
+      console.log('done')
+    })
+
+   var r = await QRCode.toDataURL( JSON.stringify(resultado));
+   console.log(r);
+   resultado.qrurl=r;
+   return resultado;
     
-    return resultado;
+    
     //return this.appService.getHello();
   }
 }
